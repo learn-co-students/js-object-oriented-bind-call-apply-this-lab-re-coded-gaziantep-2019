7b6c558f6ca3e543961f9ed9775726e825a1cbcd
Ali Mert Uytun <uytunmert801@gmail.com>
2019-08-05 08:12:06 +0000

    solved

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-//Your code here
+function justInvoke(fn){
+  return fn();
+}
+function setThisWithCall(fn, thisValue, arg){
+  return fn.call(thisValue, arg);
+}
+function setThisWithApply(fn, thisValue, args){
+  const arr = Array.prototype.slice.call(args);
+  return fn.apply(thisValue, arr);
+}
+function returnNewFunctionOf(functionToBeCopied, thisValue){
+  const newFunction = functionToBeCopied.bind(thisValue);
+  return newFunction;
+}
